d82980641e61d41be16ab9340275920e6359e8c2
unknown <Tech User@prs-L7-bshanley.providence.col>
2014-09-29 22:03:27 -0400

    initial

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "css"
+sass_dir = "public/sass"
+images_dir = "public/images"
+javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -20,6 +20,6 @@ javascripts_dir = "js"
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+#sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
